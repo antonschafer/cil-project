10e14af1bdf109bef89ccaae2ba12199225eb55e
Anton Schäfer <scanton@ethz.ch>
2022-06-28 16:15:16 +0200

    Debugging and sample job

--- train.py
+++ train.py
@@ -9,6 +9,9 @@ from utils import get_base_datasets, get_bert_config
 from pytorch_lightning.loggers import WandbLogger
 import wandb
 
+DEBUG_TRAINER_ARGS = {"limit_train_batches": 10,
+                      "limit_val_batches": 5}
+
 
 def train(config, module):
     model = module(config=config)
@@ -18,8 +21,11 @@ def train(config, module):
 
     callbacks = [EarlyStopping(monitor="val_loss", mode="min"),
                  ModelCheckpoint(monitor='val_loss', dirpath=wandb.run.dir, filename="model")]
+
+    extra_args = DEBUG_TRAINER_ARGS if config["debug"] else {}
     trainer = pl.Trainer(max_epochs=config['nepochs'], gpus=config["gpus"], callbacks=callbacks,
-                         check_val_every_n_epoch=config['val_freq'], gradient_clip_val=1, logger=wandb_logger)
+                         check_val_every_n_epoch=config['val_freq'], gradient_clip_val=1, logger=wandb_logger,
+                         **extra_args)
 
     train_set, val_set, _, test_set = get_base_datasets(config)
 
@@ -47,7 +53,10 @@ if __name__ == '__main__':
     parser.add_argument('--val_freq', type=int, default=1)
     parser.add_argument('--lr', type=float, default=2e-5)
     parser.add_argument('--batch_size', type=int, default=16)
+
     parser.add_argument('--full_data', action='store_true')
+    parser.add_argument('--debug', action='store_true',
+                        help="only run a few batches")
 
     args = parser.parse_args()
     config, module = get_bert_config(args)
