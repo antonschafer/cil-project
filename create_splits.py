47050ba9ce48f80b3d2fcc2f33ee76942f57d97c
Anton Schäfer <scanton@ethz.ch>
2022-07-23 13:20:43 +0200

    Keep inconsitenly labeled samples to not artificially increase val accuracies

--- create_splits.py
+++ create_splits.py
@@ -21,79 +21,37 @@ def read_txt(filename):
         data = [x for x in data if x != ""]
     return set(data)
 
-def split_csv_small():
+def split_csv(full, val_size, val_final_size):
 
+    suffix = "_full" if full else ""
 
-    data_neg = list(read_txt('./twitter-datasets/train_neg.txt'))
-    data_pos = list(read_txt('./twitter-datasets/train_pos.txt'))
-
-    all_x = data_pos + data_neg
-    all_y = [1 for _ in range(len(data_pos))] + [0 for _ in range(len(data_neg))]
-    all_data = {'texts':all_x,'labels':all_y}
-    df = pd.DataFrame(all_data)
-    df = shuffle(df)
-    print("All data shape",df.shape)
-    #Size of all the data is 
-    val_size = 10000
-    val_final_size = 5000
-    train_size = df.shape[0] - (val_final_size + val_size)
-    
-    print("Train, val, val final sizes",train_size, val_size, val_final_size)
-    df_train = df[: train_size]
-    df_val = df[train_size: train_size+val_size]
-    df_val_final = df[train_size+val_size:]
-
-    df_train = df_train[ ~ df_train["texts"].isin(df_val["texts"])]
-    df_train = df_train[ ~ df_train["texts"].isin(df_val_final["texts"])]
-    df_val = df_val[ ~ df_val["texts"].isin(df_val_final["texts"])]
-
-    print("Train shape",df_train.shape)
-    print("Val shape",df_val.shape)
-    print("val final shape",df_val_final.shape)
-
-    df_train.to_csv("./twitter-datasets/small_train.csv",index=False)
-    df_val.to_csv("./twitter-datasets/small_val.csv",index=False)
-    df_val_final.to_csv("./twitter-datasets/small_val_final.csv",index=False)
-
-    return
-
-
-def split_csv_full():
-
-
-    data_neg = list(read_txt('./twitter-datasets/train_neg_full.txt'))
-    data_pos = list(read_txt('./twitter-datasets/train_pos_full.txt'))
+    data_neg = list(read_txt('./twitter-datasets/train_neg{}.txt'.format(suffix)))
+    data_pos = list(read_txt('./twitter-datasets/train_pos{}.txt'.format(suffix)))
 
     all_x = data_pos + data_neg
     all_y = [1 for _ in range(len(data_pos))] + [0 for _ in range(len(data_neg))]
     all_data = {'texts':all_x,'labels':all_y}
     df = pd.DataFrame(all_data)
     df = shuffle(df)
-    print("All data shape",df.shape)
-    #Size of all the data is 
-    val_size = 100000
-    val_final_size = 50000
+    print("Total number of tweets:", len(df))
+    print("Overlap between pos and neg:", len(set(data_pos).intersection(data_neg)))
+
     train_size = df.shape[0] - (val_final_size + val_size)
     print("Train, val, val final sizes",train_size, val_size, val_final_size)
     df_train = df[: train_size]
     df_val = df[train_size: train_size+val_size]
     df_val_final = df[train_size+val_size:]
 
-    df_train = df_train[ ~ df_train["texts"].isin(df_val["texts"])]
-    df_train = df_train[ ~ df_train["texts"].isin(df_val_final["texts"])]
-    df_val = df_val[ ~ df_val["texts"].isin(df_val_final["texts"])]
-    print("Train shape",df_train.shape)
-    print("Val shape",df_val.shape)
-    print("val final shape",df_val_final.shape)
-    df_train.to_csv("./twitter-datasets/full_train.csv",index=False)
-    df_val.to_csv("./twitter-datasets/full_val.csv",index=False)
-    df_val_final.to_csv("./twitter-datasets/full_val_final.csv",index=False)
+    prefix = "full" if full else "small"
+    df_train.to_csv("./twitter-datasets/{}_train.csv".format(prefix),index=False)
+    df_val.to_csv("./twitter-datasets/{}_val.csv".format(prefix),index=False)
+    df_val_final.to_csv("./twitter-datasets/{}_val_final.csv".format(prefix),index=False)
 
     return
 
 
 if __name__ == '__main__':
-
-    split_csv_full()       
-
-    split_csv_small()                                                                                                          
+    print("Generating full datasplit")
+    split_csv(full=True, val_size=100000, val_final_size=50000)
+    print("\nGenerating small datasplit")
+    split_csv(full=False, val_size=10000, val_final_size=5000)
